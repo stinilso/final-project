94a43906c9b452516f78ea8b47955a9207ce6e88
Stine <stine@sandornes.com>
2020-12-02 14:17:47 -0500

    button test

--- javascript/content.js
+++ javascript/content.js
@@ -1,2 +1,7 @@
 // content.js
 alert("Hi! This is the annoying extension prototype test.");
+
+$(browser.icon).click(function() {
+  $(body).append("button");
+  $("button").css("background-color", "#d5b941");
+});
