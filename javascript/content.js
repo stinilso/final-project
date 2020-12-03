2c691fb404314549d02f036433c7c2a5e194fa16
Stine <stine@sandornes.com>
2020-12-02 21:22:24 -0500

    button css done

--- javascript/content.js
+++ javascript/content.js
@@ -1,7 +1,9 @@
 // content.js
-alert("Hi! This is the annoying extension prototype test.");
+// alert("Hi! This is the annoying extension prototype test.");
 
-$(browser.icon).click(function() {
-  $(body).append("button");
-  $("button").css("background-color", "#d5b941");
+$("body").append("<button style='width:130px; height:50px; z-index:2147483646; background-color:#d5b941; border-color:#e26e5c; border-width:4px; border-radius:10px; position:fixed; bottom:25px; right:25px; color:#723d46; font-size:18px; font-weight:bold; font-family:'Helvetica';' id='start-button'>Click here!</button>");
+$(document).on("click", "#start-button", function() {
+  alert("test");
 });
+
+// "<button style='z-index:2147483646; background-color:red; position:fixed; bottom:0px; right:0px;' id='stine-button'>Click here!</button>"
