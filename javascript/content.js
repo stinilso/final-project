0db73df50a3f20d962a19bf5c1fc1dfb84ec4817
Stine <stine@sandornes.com>
2020-12-10 20:00:22 -0500

    timer and scroll check work

--- javascript/content.js
+++ javascript/content.js
@@ -1,9 +1,34 @@
-// content.js
-// alert("Hi! This is the annoying extension prototype test.");
+
+var maxTime = 5000;
+var intervalTime = 1000;
+var currentTime = 0;
+var calculateScroll;
+var displayResultsTimerID;
+
+var currentPosition;
+var totalDistanceScrolled;
+
+
+// 1 second interval timer
+function calculateScroll() {
+    var newPosition = window.pageYOffset;
+    var intervalDistanceScrolled = Math.abs(newPosition - currentPosition);
+    totalDistanceScrolled += intervalDistanceScrolled;
+    currentPosition = newPosition;
+}
+
+// 5 minute timer
+function displayResults() {
+  clearInterval(checkTimerID);
+  alert(totalDistanceScrolled);
+}
+
 
 $("body").append("<button style='width:130px; height:50px; z-index:2147483646; background-color:#d5b941; border-color:#e26e5c; border-width:4px; border-radius:10px; position:fixed; bottom:25px; right:25px; color:#723d46; font-size:18px; font-weight:bold; font-family:'Helvetica';' id='start-button'>Click here!</button>");
 $(document).on("click", "#start-button", function() {
-  alert("test");
+  //alert("test");
+  checkTimerID = setInterval(calculateScroll, intervalTime);
+  displayResultsTimerID = setTimeout(displayResults, maxTime);
+  currentPosition = window.pageYOffset;
+  totalDistanceScrolled = 0;
 });
-
-// "<button style='z-index:2147483646; background-color:red; position:fixed; bottom:0px; right:0px;' id='stine-button'>Click here!</button>"
